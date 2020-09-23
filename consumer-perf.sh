a918cbbd2b566ddf0192225fc99be934fb6d9ee4
Markus Härnvi <markus@harnvi.net>
2020-09-23 15:25:49 +0200

    perf changes

--- consumer-perf.sh
+++ consumer-perf.sh
@@ -2,8 +2,8 @@
 
 /home/marvi/Documents/kafka/bin/kafka-consumer-perf-test.sh \
   --broker-list 188.166.86.73:9092 \
-  --messages 150000 \
-  --topic benchmark-2 \
-  --threads 333 \
+  --messages 1500000 \
+  --topic benchmark-3 \
+  --threads 3 \
   --print-metrics \
   --show-detailed-stats
--- producer-perf.sh
+++ producer-perf.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-/home/marvi/Documents/kafka_2.12-2.6.0/bin/kafka-producer-perf-test.sh --topic benchmark-2 \
---num-records 150000 \
+/home/marvi/Documents/kafka_2.12-2.6.0/bin/kafka-producer-perf-test.sh --topic benchmark-3 \
+--num-records 1500000 \
 --record-size 600 \
 --throughput -1 \
 --producer-props \
 acks=1 \
 bootstrap.servers=188.166.86.73:9092 \
+buffer.memory=67108864 \
 compression.type=snappy \
-batch.size=256
-
+batch.size=65537 \
+linger.ms=5
 
 #~/Documents/kafka_2.12-2.6.0/bin/kafka-run-class.sh \
 #  kafka.tools.EndToEndLatency \
